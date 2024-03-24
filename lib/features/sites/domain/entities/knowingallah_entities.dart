d38541e17e4588c1cec01cda50af433a66ef91d1
mohammedalsamet2024 <161654898+mohammedalsamet2024@users.noreply.github.com>
2024-03-24 16:18:19 -0700

    Create knowingallah_entities.dart
    create knowingallah_entities

---
+++ lib/features/sites/domain/entities/knowingallah_entities.dart
@@ -0,0 +1,11 @@
+import 'package:elresala/features/sites/domain/entities/islam_for_christians_entities.dart';
+
+class KnowingAllahEntity {
+  final String name;
+  final List<IslamForChristiansEntities> catigory;
+
+  const KnowingAllahEntity({
+    required this.name,
+    required this.catigory, 
+  });
+}
